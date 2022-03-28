a9abfac9d084baca226887ab8b403d3f97d34075
Vincent Josse <vincent.josse@telecom-paris.fr>
2022-03-29 00:13:53 +0200

    Ingesting data stream of bids and aks for depth 20 done

--- ingest-data.py
+++ ingest-data.py
@@ -1,13 +1,50 @@
 import time
 import json
 import urllib.request
+from aiohttp import ThreadedResolver
 from kafka import KafkaProducer
 from binance.client import Client
+from binance import ThreadedWebsocketManager
+from more_itertools import last
+
+producer = KafkaProducer(bootstrap_servers=["localhost:9092"])
+
+topic_name = "depth20-btc"
+
+#BASED ON: https://github.com/SpiralDevelopment/crypto-hft-data/blob/master/exchange_sockets/binance_websocket.py
 
 
-producer = KafkaProducer(bootstrap_servers="localhost:9092")
 
-topic_name = "velib-stations"
+def broadcast_kafka(msg): 
+    stream = msg.get('stream', '')
+    stream_parts = stream.split('@')
+    data = msg.get('data')
+
+    bids = data.get('bids', [])
+    asks = data.get('asks', [])
+
+    if len(bids) == 0 and len(asks) == 0:
+        bids = data.get('b', [])
+        asks = data.get('a', [])
+
+    if len(bids) > 0 or len(asks) > 0:
+        time_now = int(time.time())
+        producer.send(topic_name, json.dumps({"time": time_now, "bids":bids, "asks": asks}).encode())
+        
+def handle_message(msg):
+        broadcast_kafka(msg)
+
+
+client = Client('', '')
+
+
+streams = ['btcbusd@depth20@100ms']
+
+twm = ThreadedWebsocketManager()    
+twm.start()
+depth_stream_name = twm.start_multiplex_socket(callback=handle_message, streams=streams)
+
+
 
 with open("./API_KEY", 'r') as f:
     API_KEY = f.readline()
@@ -15,5 +52,3 @@ with open("./API_KEY", 'r') as f:
 
 uri = "wss://stream.binance.com:9443" 
 
-while True:
-    time.sleep(1)
